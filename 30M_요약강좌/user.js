c746d2392cd287bd25a4e3f9d08e931c88ab272f
hyunjun Yang <yhj0439@nate.com>
2022-04-17 18:28:55 +0900

    jquery 사용 연습

---
+++ 30M_요약강좌/user.js
@@ -0,0 +1,119 @@
+var user = [
+    {
+      "_id": "625bdb273d4d7090fd3c1da0",
+      "age": 39,
+      "eyeColor": "green",
+      "name": "Bridgett Castro",
+      "gender": "female",
+      "email": "bridgettcastro@quinex.com",
+      "phone": "+1 (963) 401-2975"
+    },
+    {
+      "_id": "625bdb277dd4977618c4cd76",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Lavonne Nelson",
+      "gender": "female",
+      "email": "lavonnenelson@quinex.com",
+      "phone": "+1 (916) 450-3033"
+    },
+    {
+      "_id": "625bdb279ffa6b90c6e803fe",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Paige Patel",
+      "gender": "female",
+      "email": "paigepatel@quinex.com",
+      "phone": "+1 (885) 455-3745"
+    },
+    {
+      "_id": "625bdb27a5f83173e0a85e3f",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": "Kimberly Hopper",
+      "gender": "female",
+      "email": "kimberlyhopper@quinex.com",
+      "phone": "+1 (931) 501-3676"
+    },
+    {
+      "_id": "625bdb274493fbd5879234f1",
+      "age": 39,
+      "eyeColor": "brown",
+      "name": "Logan Rasmussen",
+      "gender": "male",
+      "email": "loganrasmussen@quinex.com",
+      "phone": "+1 (868) 560-2816"
+    },
+    {
+      "_id": "625bdb27c3a7bb4a91d27af4",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Mercer Chapman",
+      "gender": "male",
+      "email": "mercerchapman@quinex.com",
+      "phone": "+1 (837) 433-3177"
+    },
+    {
+      "_id": "625bdb27f2facdc8edaad592",
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Jerry Hopkins",
+      "gender": "female",
+      "email": "jerryhopkins@quinex.com",
+      "phone": "+1 (872) 432-2832"
+    },
+    {
+      "_id": "625bdb27a29dd7885c75faca",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Maynard Bowen",
+      "gender": "male",
+      "email": "maynardbowen@quinex.com",
+      "phone": "+1 (889) 516-2823"
+    },
+    {
+      "_id": "625bdb2762f475fd9a6dae23",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Belinda Holt",
+      "gender": "female",
+      "email": "belindaholt@quinex.com",
+      "phone": "+1 (993) 458-2070"
+    },
+    {
+      "_id": "625bdb2770dc7fbc8b8d4e52",
+      "age": 33,
+      "eyeColor": "blue",
+      "name": "Yolanda Fitzgerald",
+      "gender": "female",
+      "email": "yolandafitzgerald@quinex.com",
+      "phone": "+1 (942) 563-3452"
+    },
+    {
+      "_id": "625bdb273e9ada9e7276ad08",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": "Roach Cortez",
+      "gender": "male",
+      "email": "roachcortez@quinex.com",
+      "phone": "+1 (843) 414-3441"
+    },
+    {
+      "_id": "625bdb2783304d2f1bf3366b",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Scott Page",
+      "gender": "male",
+      "email": "scottpage@quinex.com",
+      "phone": "+1 (894) 417-2245"
+    },
+    {
+      "_id": "625bdb2741f3143a49a8cb57",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Wilda Crane",
+      "gender": "female",
+      "email": "wildacrane@quinex.com",
+      "phone": "+1 (889) 562-2565"
+    }
+  ]
